8d7ee7beb740b991d3c2853c4c524d6afc8fc9a9
Tramscan <kidscline02@gmail.com>
2019-10-01 21:43:46 -0400

    Added arduinoInit
    Added Arduino program to be uploaded that will blink a light when messages are received from the TestFrames java application. The String testSendString should be the same as testSendString in TestFrames.java as that's the message that will allow the blinking to happen.

---
+++ arduinoInit/arduinoInit.ino
@@ -0,0 +1,34 @@
+long duration;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  ledBlink();
+  delay(5000);
+
+}
+//USE JSSC
+void loop() {
+  // put your main code here, to run repeatedly:
+  String serialMat="G0 X100";
+  while(Serial.available()>0){
+    
+    if(Serial.readString() == serialMat){
+      ledBlink();
+    }
+
+  }
+  
+
+}
+
+void ledBlink(){
+  for(int i =0;i<6;i++){
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
+  }
+  digitalWrite(13,LOW);
+}
+
